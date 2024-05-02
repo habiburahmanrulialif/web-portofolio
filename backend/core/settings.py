2715e17392f5bf035b71c8d337767f0eaa01fef2
habiburahmanrulialif <habibalif865@gmail.com>
2024-05-01 21:18:41 -0700

    checking sum stuff

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'website',
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173/']
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
