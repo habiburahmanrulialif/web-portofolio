8f0bf95137f26cef513e7a966dd50e2173d11592
habiburahmanrulialif <habibalif865@gmail.com>
2024-05-21 06:45:32 +0700

    adding assets for project

--- frontend/src/components/portofolio/Portofolio.jsx
+++ frontend/src/components/portofolio/Portofolio.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./portofolio.css";
 import ProjectItem from "./ProjectItem";
 import testImg from "../../assets/home-background.jpg";
+import Chatter50 from "../../assets/Chatter50.jpg";
+import Network50 from "../../assets/Network50.jpg";
+import Mail50 from "../../assets/Mail50.jpg";
+import Commerce50 from "../../assets/Chatter50.jpg";
+import Wiki50 from "../../assets/Wiki50.jpg";
 
 const Portofolio = () => {
     return (
@@ -11,17 +16,14 @@ const Portofolio = () => {
                     PROJECT
                 </h1>
                 <p className="titleDescription">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Aspernatur, at nulla? Doloribus eos nobis veritatis eligendi
-                    illo! Necessitatibus voluptatem ex deleniti unde quae omnis
-                    dolorum. Quas optio ex perferendis temporibus?
+                    Here are some of my past project that i have done in order to finish harvard CS50 classes.
                 </p>
                 <div className="projectItem">
-                    <ProjectItem img={testImg} title="Chatter50" link="https://chatter50.up.railway.app" />
-                    <ProjectItem img={testImg} title="Network50" link="https://network50.up.railway.app" />
-                    <ProjectItem img={testImg} title="Mail50" link="https://mail50.up.railway.app" />
-                    <ProjectItem img={testImg} title="Commerce50" link="https://commerce50.up.railway.app" />
-                    <ProjectItem img={testImg} title="Wiki50" link="https://wiki50.up.railway.app" />
+                    <ProjectItem img={Chatter50} title="Chatter50" link="https://chatter50.up.railway.app" />
+                    <ProjectItem img={Network50} title="Network50" link="https://network50.up.railway.app" />
+                    <ProjectItem img={Mail50} title="Mail50" link="https://mail50.up.railway.app" />
+                    <ProjectItem img={Commerce50} title="Commerce50" link="https://commerce50.up.railway.app" />
+                    <ProjectItem img={Wiki50} title="Wiki50" link="https://wiki50.up.railway.app" />
                 </div>
             </div>
         </div>
