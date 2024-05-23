96181ba1a4d1abf3211bbb988886ce7ef7af0829
habiburahmanrulialif <habibalif865@gmail.com>
2024-05-24 06:19:39 +0700

    adding details button on portofolio

--- frontend/src/components/portofolio/Portofolio.jsx
+++ frontend/src/components/portofolio/Portofolio.jsx
@@ -19,11 +19,11 @@ const Portofolio = () => {
                     Here are some of my past project that i have done in order to finish harvard CS50 classes.
                 </p>
                 <div className="projectItem">
-                    <ProjectItem img={Chatter50} title="Chatter50" link="https://chatter50.up.railway.app" />
-                    <ProjectItem img={Network50} title="Network50" link="https://network50.up.railway.app" />
-                    <ProjectItem img={Mail50} title="Mail50" link="https://mail50.up.railway.app" />
-                    <ProjectItem img={Commerce50} title="Commerce50" link="https://commerce50.up.railway.app" />
-                    <ProjectItem img={Wiki50} title="Wiki50" link="https://wiki50.up.railway.app" />
+                    <ProjectItem img={Chatter50} title="Chatter50" link="https://chatter50.up.railway.app" githubLink="https://github.com/habiburahmanrulialif/Chatter" />
+                    <ProjectItem img={Network50} title="Network50" link="https://network50.up.railway.app" githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/NETWORK" />
+                    <ProjectItem img={Mail50} title="Mail50" link="https://mail50.up.railway.app" githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/MAIL" />
+                    <ProjectItem img={Commerce50} title="Commerce50" link="https://commerce50.up.railway.app" githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/COMMERCE" />
+                    <ProjectItem img={Wiki50} title="Wiki50" link="https://wiki50.up.railway.app" githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/WIKI" />
                 </div>
             </div>
         </div>
