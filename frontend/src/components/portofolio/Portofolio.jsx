32b37eca31d0b8e550ea202a15130f4f488594d7
habiburahmanrulialif <habibalif865@gmail.com>
2024-06-07 09:19:10 +0700

    Adding tech used on project

--- frontend/src/components/portofolio/Portofolio.jsx
+++ frontend/src/components/portofolio/Portofolio.jsx
@@ -7,25 +7,59 @@ import Network50 from "../../assets/Network50.png";
 import Mail50 from "../../assets/Mail50.png";
 import Commerce50 from "../../assets/Commerce50.png";
 import Wiki50 from "../../assets/Wiki50.png";
-import ReactionTimer from "../../assets/reaction-timer.png"
+import ReactionTimer from "../../assets/reaction-timer.png";
 
 const Portofolio = () => {
     return (
         <div className="project">
             <div id="project">
-                <h1 className="title" >
-                    PROJECT
-                </h1>
+                <h1 className="title">PROJECT</h1>
                 <p className="titleDescription">
-                    Here are some of my past project that i have done in order to finish harvard CS50 classes.
+                    Here are some of my past project that i have done in order
+                    to finish harvard CS50 classes.
                 </p>
                 <div className="projectItem">
-                    <ProjectItem img={Chatter50} title="Chatter50" link="https://chatter50.up.railway.app" githubLink="https://github.com/habiburahmanrulialif/Chatter" />
-                    <ProjectItem img={Network50} title="Network50" link="https://network50.up.railway.app" githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/NETWORK" />
-                    <ProjectItem img={Mail50} title="Mail50" link="https://mail50.up.railway.app" githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/MAIL" />
-                    <ProjectItem img={Commerce50} title="Commerce50" link="https://commerce50.up.railway.app" githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/COMMERCE" />
-                    <ProjectItem img={Wiki50} title="Wiki50" link="https://wiki50.up.railway.app" githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/WIKI" />
-                    <ProjectItem img={ReactionTimer} title="Reaction timer game" link="https://vue-project-production.up.railway.app" githubLink="https://github.com/habiburahmanrulialif/Vue-Project/tree/main/reaction-timer-game" />
+                    <ProjectItem
+                        img={Chatter50}
+                        title="Chatter50"
+                        link="https://chatter50.up.railway.app"
+                        githubLink="https://github.com/habiburahmanrulialif/Chatter"
+                        tech="Django + vanilla js"
+                    />
+                    <ProjectItem
+                        img={Network50}
+                        title="Network50"
+                        link="https://network50.up.railway.app"
+                        githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/NETWORK"
+                        tech="Django + vanilla js"
+                    />
+                    <ProjectItem
+                        img={Mail50}
+                        title="Mail50"
+                        link="https://mail50.up.railway.app"
+                        githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/MAIL"
+                        tech="Django + vanilla js"
+                    />
+                    <ProjectItem
+                        img={Commerce50}
+                        title="Commerce50"
+                        link="https://commerce50.up.railway.app"
+                        githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/COMMERCE"
+                    />
+                    <ProjectItem
+                        img={Wiki50}
+                        title="Wiki50"
+                        link="https://wiki50.up.railway.app"
+                        githubLink="https://github.com/habiburahmanrulialif/CS50Web/tree/main/WIKI"
+                        tech="Django + vanilla js"
+                    />
+                    <ProjectItem
+                        img={ReactionTimer}
+                        title="Reaction timer game"
+                        link="https://vue-project-production.up.railway.app"
+                        githubLink="https://github.com/habiburahmanrulialif/Vue-Project/tree/main/reaction-timer-game"
+                        tech="Vue js"
+                    />
                 </div>
             </div>
         </div>
--- frontend/src/components/portofolio/ProjectItem.jsx
+++ frontend/src/components/portofolio/ProjectItem.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./projectItem.css";
 
-const ProjectItem = ({ img, title, link, githubLink }) => {
+const ProjectItem = ({ img, title, link, githubLink, tech }) => {
     return (
         <div className="projectItem-Item">
             <img src={img} alt="/" className="projectImg" />
             <div className="projectDetailContainer">
                 <h3 className="projectTitle">{title}</h3>
+                <p>{tech}</p>
                 <a href={link} target="_blank" className="projectLink">LIVE DEMO</a>
                 <a href={githubLink} target="_blank" className="githubLink">Details</a>
             </div>
